0bea67eccc2e4214afa09898f187298d40812a5f
Sandhi Karunia Sugihartana <sandhi372@gmail.com>
2023-07-14 05:23:56 +0700

    Create CCRejected.blade.php

---
+++ resources/views/mail/CCRejected.blade.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+</head>
+<body>
+
+  <div style="text-align: justify">
+    {{-- Kata sambutan --}}
+    <div>
+      <p><span style="font-style: italic; font-weight: 700;">Unfortunately,</span> <br> Your registration for the Coaching Clinic 2023 is not successfully processed. There can be several possible reasons why the registration is not successful.</p>
+    </div>
+
+    {{-- Info --}}
+    <div>
+        <p style="margin-bottom: 0;">For further information, please contact:</p> 
+        <table cellpadding="0" cellspacing="0">
+          <tr style="text-align: left">
+            <td>E-mail</td>
+            <td style="display: inline-block; margin: 0 10px; margin-left: 18px;">:</td>
+            <td>info.DBCC2023@gmail.com</td>
+          </tr>
+        </table>
+        <table cellpadding="0" cellspacing="0" style="margin-top: 10px">
+          <tr style="text-align: left;">
+            <td>Name</td>
+            <td style="display: inline-block; margin: 0 10px;">:</td>
+            <td>Natalia Karlina Ginting</td>
+          </tr>
+          <tr style="text-align: left;">
+            <td>ID LINE</td>
+            <td style="display: inline-block; margin: 0 10px;">:</td>
+            <td>nalalalice</td>
+          </tr>
+          <tr style="text-align: left;">
+            <td>Phone</td>
+            <td style="display: inline-block; margin: 0 10px;">:</td>
+            <td>082375051837</td>
+          </tr>
+        </table>
+        <table cellpadding="0" cellspacing="0" style="margin-top: 10px">
+          <tr style="text-align: left">
+            <td>Name</td>
+            <td style="display: inline-block; margin: 0 10px;">:</td>
+            <td>Cindy Meilani Margaretha</td>
+          </tr>
+          <tr style="text-align: left;">
+            <td>ID LINE</td>
+            <td style="display: inline-block; margin: 0 10px;">:</td>
+            <td>cindymeilanii</td>
+          </tr>
+          <tr style="text-align: left;">
+            <td>Phone</td>
+            <td style="display: inline-block; margin: 0 10px;">:</td>
+            <td>0895372267150</td>
+          </tr>
+        </table>
+        <p style="margin-top: 16px">Active Hours : <br> 10.00 – 21.00 UTC+07:00</p>
+    </div>
+
+    {{-- Best Regards --}}
+    <div style="text-align: left; font-weight: 400; margin-top: 30px">
+      <span style="font-style: italic; font-size: 16px">Best Regards,</span>
+      <pre style="font-family: Arial, Helvetica, sans-serif; margin-top: 0; margin-bottom: 0; font-style: italic;">
+<span style="font-weight: 700; font-size: 16px;">Diponegoro Business Case Competition 2023<br>The 15th ENFUTION 2023</span>
+<span style="font-size: 16px;">Management Student Association</span>
+<span style="font-size: 16px;">Faculty of Economics and Business,</span>
+<span style="font-size: 16px;">Diponegoro University, Semarang, Indonesia</span>
+</pre>
+<span style="margin-top: 0; font-size: 16px;">dbcc2023@gmail.com | www.enfution15.com</span>
+      {{-- <img src="{{ $message->embed('images/LogoSemnas2.png') }}" alt="" width="200"> --}}
+    </div>
+  </div>
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script>
+</body>
+</html>
