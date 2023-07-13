0ced9ba71a9744e8ef23320b5d50b345f00a2302
Sandhi Karunia Sugihartana <sandhi372@gmail.com>
2023-07-14 05:18:11 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,12 @@
 <?php
 
+use App\Http\Controllers\CCAdminController;
+use App\Http\Controllers\CSAdminController;
+use App\Http\Controllers\CSParticipantController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\DBCCAdminController;
+use App\Http\Controllers\DBCCParticipantController;
+use App\Http\Controllers\DBCCTransactionController;
 use App\Http\Controllers\MailController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ReportController;
@@ -13,7 +20,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use PHPUnit\Framework\Attributes\Group;
-
+use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
 Route::get('/', function () {
     return Inertia::render('HomePage');
@@ -42,6 +49,42 @@ Route::controller(SemnasTransactionController::class)->middleware('payment-semna
 
 // Akhir Semnas
 
+// Awal DBCC | CS | CC
+
+Route::get('/DBCC', [DBCCParticipantController::class, 'index'])->name('dbcc.main');
+
+Route::controller(DBCCParticipantController::class)->middleware('url-dbcc')->group(function () {
+    // Load Form
+    Route::get('/registration-dbcc', 'create')->name('dbcc.form-summit');
+    // Route::get('/registration-coaching-session', 'create')->name('dbcc.form-cs');
+    // Route::get('/registration-coaching-clinic', 'create')->name('dbcc.form-cc');
+    // Process Form
+    Route::post('/registration-dbcc', 'store');
+    // Route::post('/registration-coaching-session', 'store');
+    // Route::post('/registration-coaching-clinic', 'store');
+});
+
+Route::controller(CSParticipantController::class)->middleware('url-cs')->group(function () {
+    // Load Form
+    Route::get('/registration-coaching-session', 'create')->name('dbcc.form-cs');
+    // Process Form
+    Route::get('/registration-coaching-session', 'store');
+});
+
+Route::controller(CCParticipantController::class)->middleware('url-cc')->group(function () {
+    // Load Form
+    Route::get('/registration-coaching-clinic', 'create')->name('dbcc.form-cc');
+    // Process Form
+    Route::get('/registration-coaching-clinic', 'store');
+});
+
+Route::controller(DBCCTransactionController::class)->middleware('payment-dbcc')->group(function () {
+    Route::get('/payment-confirmation-dbcc', 'transaction')->name('dbcc.payment-confirmation');
+    Route::post('/payment-confirmation-dbcc', 'save');
+});
+
+// Akhir DBCC | CS | CC
+
 // Awal Subscribe
 
 Route::post('/subscribe', [NewsletterController::class, 'subscribe'])->name('subscribe');
@@ -50,45 +93,72 @@ Route::post('/subscribe', [NewsletterController::class, 'subscribe'])->name('sub
 
 // Awal DBCC
 
-Route::get('/DBCC', function () {
-    return Inertia::render('DBCCPage');
-})->name('dbcc');
+    // Route::get('/DBCC', function () {
+    //     return Inertia::render('DBCCPage');
+    // })->name('dbcc');
 
-Route::get('/registration-dbcc', function () {
-    // return Inertia::render('DBCC/FormDBCC');
-    return redirect()->route('dbcc');
-})->name('form-dbcc');
+    // Route::get('/registration-dbcc', function () {
+    //     return Inertia::render('DBCC/FormDBCC');
+    //     // return redirect()->route('dbcc');
+    // })->name('form-dbcc');
 
 Route::get('/registration-coaching-session', function () {
-    // return Inertia::render('DBCC/FormSession');
-    return redirect()->route('dbcc');
+    return Inertia::render('DBCC/FormSession');
+    // return redirect()->route('dbcc');
 })->name('form-session');
 
 Route::get('/registration-coaching-clinic', function () {
-    // return Inertia::render('DBCC/FormClinic');
-    return redirect()->route('dbcc');
+    return Inertia::render('DBCC/FormClinic');
+    // return redirect()->route('dbcc');
 })->name('form-clinic');
 
-Route::get('/payment-confirmation-dbcc', function () {
-    // return Inertia::render('DBCC/PaymentConfirmation');
-    return redirect()->route('dbcc');
-})->name('payment-confirmation');
+// Route::get('/payment-confirmation-dbcc', function () {
+//     return Inertia::render('DBCC/PaymentConfirmation');
+//     // return redirect()->route('dbcc');
+// })->name('payment-confirmation');
 
 // Akhir DBCC
 
 // Awal Dashboard
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('/dashboard', [SemnasAdminController::class, 'index'])->name('dashboard');
+    // Semnas
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
     Route::get('/dashboard/national-seminar', [SemnasAdminController::class, 'semnas'])->name('dashboard-semnas');
-    Route::get('/dashboard/DBCC', [SemnasAdminController::class, 'DBCC'])->name('dashboard-DBCC');
-    Route::get('/dashboard/subscriber', [NewsletterController::class, 'index'])->name('dashboard-subscriber');
-    Route::get('/newsLetter', [NewsletterController::class, 'newsLetter'])->name('newsLetter');
-    Route::post('/sendNewsLetter', [NewsletterController::class, 'sendNewsLetter']);
-    Route::get('/getPaymentSlip', [SemnasAdminController::class, 'payment']);
+    Route::get('/getPaymentSlipSemnas', [SemnasAdminController::class, 'payment']);
     Route::get('/detail-semnas-participant/{participant}', [SemnasAdminController::class, 'detail']);
     Route::get('/sendVerif/{participant}', [MailController::class, 'sendVerif']);
     Route::get('/rejected/{transaction}', [MailController::class, 'sendRejected']);
     Route::get('/printSemnas', [ReportController::class, 'printVerif']);
+
+    // DBCC
+    Route::get('/dashboard/DBCC', [DBCCAdminController::class, 'index'])->name('dashboard-DBCC');
+    Route::get('/getPaymentSlipDBCC', [DBCCAdminController::class, 'payment']);
+    Route::get('/detail-dbcc-participant/{team}', [DBCCAdminController::class, 'detail']);
+    Route::get('/DBCCVerif/{team}', [MailController::class, 'DBCCVerif']);
+    Route::get('/DBCCRejected/{team}', [MailController::class, 'DBCCRejected']);
+    Route::get('/printDBCC', [ReportController::class, 'printDBCCVerif']);
+
+    // Coaching Session
+    Route::get('/dashboard/coaching-session', [CSAdminController::class, 'index'])->name('dashboard-CS');
+    Route::get('/getPaymentSlipCS', [CSAdminController::class, 'payment']);
+    Route::get('/detail-cs-participant/{participant}', [CSAdminController::class, 'detail']);
+    Route::get('/CSVerif/{participant}', [MailController::class, 'CSVerif']);
+    Route::get('/CSRejected/{participant}', [MailController::class, 'CSRejected']);
+    Route::get('/printCS', [ReportController::class, 'printCSVerif']);
+
+    // Coaching Clinic
+    Route::get('/dashboard/coaching-clinic', [CCAdminController::class, 'index'])->name('dashboard-CC');
+    Route::get('/getPaymentSlipCC', [CCAdminController::class, 'payment']);
+    Route::get('/getDetailFile', [CCAdminController::class, 'file']);
+    Route::get('/detail-cc-participant/{participant}', [CCAdminController::class, 'detail']);
+    Route::get('/CCVerif/{participant}', [MailController::class, 'CCVerif']);
+    Route::get('/CCRejected/{participant}', [MailController::class, 'CCRejected']);
+    Route::get('/printCC', [ReportController::class, 'printCCVerif']);
+
+    // Newsletter
+    Route::get('/dashboard/subscriber', [NewsletterController::class, 'index'])->name('dashboard-subscriber');
+    Route::get('/newsLetter', [NewsletterController::class, 'newsLetter'])->name('newsLetter');
+    Route::post('/sendNewsLetter', [NewsletterController::class, 'sendNewsLetter']);
 });
 // Akhir Dashboard
 
