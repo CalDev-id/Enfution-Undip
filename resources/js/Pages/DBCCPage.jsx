9d6f30783aa1a79078282e8ada9d92ac721b2b22
Sandhi Karunia Sugihartana <sandhi372@gmail.com>
2023-07-14 05:24:37 +0700

    Update DBCCPage.jsx

--- resources/js/Pages/DBCCPage.jsx
+++ resources/js/Pages/DBCCPage.jsx
@@ -7,7 +7,7 @@ import Navbar from "@/Components/Navbar";
 import TestiPage from "@/Components/TestiPage";
 import Galery from "@/Components/Gallery";
 
-const DBCCPage = () => {
+const DBCCPage = (props) => {
     return (
         <section className="bg-[#FFF9EE]">
             <Head title="DBCC" />
@@ -16,6 +16,11 @@ const DBCCPage = () => {
             <Hero active={"dbcc"} />
 
             <AboutUs active={"dbcc"} />
+
+            <Events
+                ticketPrice={props.ticketPrice}
+                timeRegist={props.timeRegist}
+            />
 
             <Events hrefDbcc={"/form-dbcc"}/>
 
