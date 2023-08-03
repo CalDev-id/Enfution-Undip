76aece62021335c086ca591a5ce02bc5fd7c6176
Sandhi Karunia Sugihartana <sandhi372@gmail.com>
2023-08-04 01:09:40 +0700

    Update CSTransaction.php

--- app/Models/CSTransaction.php
+++ app/Models/CSTransaction.php
@@ -10,13 +10,15 @@ class CSTransaction extends Model
     use HasFactory;
     protected $guarded = ['id'];
     protected $table = "cs_transactions";
+    protected $with = ['cs_participant'];
 
     public function scopeFilter($query, array $filters)
     {
         $query->when(
             $filters['search'] ?? false,
             fn ($query, $search) =>
-            $query->where(fn ($query) => $query->where('account_name', 'like', '%' . $search . '%')->orWhere('account_number', 'like', '%' . $search . '%'))
+            $query->where(fn ($query) => $query->where('account_name', 'like', '%' . $search . '%')->orWhere('account_number', 'like', '%' . $search . '%'))->orWhere(fn ($query) =>
+            $query->whereHas('cs_participant', fn ($query) => $query->where('dbcc_registration_code', $search)))
         );
         $query->when(
             $filters['status'] ?? false,
