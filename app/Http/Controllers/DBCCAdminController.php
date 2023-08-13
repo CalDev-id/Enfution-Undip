0b82b827d3336dbd1fdd82639c525e9528bc3d75
Sandhi Karunia Sugihartana <sandhi372@gmail.com>
2023-08-13 15:24:43 +0700

    Update DBCCAdminController.php

--- app/Http/Controllers/DBCCAdminController.php
+++ app/Http/Controllers/DBCCAdminController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\DBCCParticipant;
+use App\Models\DBCCReferralCode;
 use App\Models\DBCCTeam;
 use App\Models\DBCCTransaction;
 use Inertia\Inertia;
@@ -24,12 +25,18 @@ class DBCCAdminController extends Controller
             ]);
         }
         $transactions = DBCCTransaction::filter(request($req))->where('status_bayar', 'PAID')->paginate(10)->withQueryString();
+        $refcodes = DBCCReferralCode::all();
         foreach ($transactions as $trx) {
-            $trx = $trx->dbcc_team;
+            $trx->dbcc_team;
+            $trx['ref_code'] =  $trx->dbcc_team->id_referral_code ? DBCCReferralCode::where('id', $trx->dbcc_team->id_referral_code)->first()->code : '';
+        }
+        foreach ($refcodes as $rc) {
+            $rc['used'] = count(DBCCTeam::where('id_referral_code', $rc->id)->get());
         }
 
         session()->put(['page' => request('page') ?? 1, 'periode' => request('periode') ?? "", 'status' => request('status') ?? ""]);
         $data['transactions'] = $transactions;
+        $data['referral_codes'] = $refcodes;
         $data['filter'] = session('periode');
         $data['info'] = session('success') ?? session('rejected') ?? "";
         $data['search'] = request('search');
