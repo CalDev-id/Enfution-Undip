d19662af3c8f501b364237c00f485f4ca6c25945
Dynavx <haical31@gmail.com>
2023-08-20 19:10:45 +0700

    update

--- resources/js/Components/Sponsors.jsx
+++ resources/js/Components/Sponsors.jsx
@@ -49,48 +49,48 @@ const Sponsors = () => {
                             <div className="flex justify-center mx-auto">
                                 <img
                                     src="images/media/1.png"
-                                    className="w-10 mr-2"
+                                    className="w-fit h-14 mr-2"
                                     alt=""
                                 />
                                 <img
                                     src="images/media/2.png"
-                                    className="w-24 mr-2"
+                                    className="w-fit h-14 mr-2"
                                     alt=""
                                 />
                                 <img
                                     src="images/media/3.png"
-                                    className="w-10"
+                                    className="w-fit h-14"
                                     alt=""
                                 />
                             </div>
                             <div className="flex justify-center mx-auto">
                                 <img
                                     src="images/media/4.png"
-                                    className="w-14 h-7"
+                                    className="w-fit h-10"
                                     alt=""
                                 />
                                 <img
                                     src="images/media/5.png"
-                                    className="w-20 h-8"
+                                    className="w-fit h-9"
                                     alt=""
                                 />
                                 <img
                                     src="images/media/6.png"
-                                    className="w-16 h-7"
+                                    className="w-fit h-10"
                                     alt=""
                                 />
                                 <img
                                     src="images/media/7.png"
-                                    className="h-7"
+                                    className="h-8 w-fit"
                                     alt=""
                                 />
                                 <img
                                     src="images/media/8.png"
-                                    className="h-7"
+                                    className="h-8 w-fit"
                                     alt=""
                                 />
                             </div>
-                            <div className="flex justify-center mx-auto">
+                            <div className="flex justify-center mx-auto my-2">
                                 <img
                                     src="images/sponsors/webinarindonesia.png"
                                     className="w-7 h-7"
@@ -134,70 +134,38 @@ const Sponsors = () => {
                                     alt=""
                                 />
                             </div>
-                            <div className="flex mx-5 my-0 md:justify-center justify-evenly">
+                            <div className="flex mx-5 my-0 md:justify-center justify-evenly flex-wrap">
                                 <img
-                                    className="w-fit md:mr-5 h-fit"
+                                    className="md:mr-2 w-fit h-7"
                                     src="images/sponsors/BEMFEB.png"
                                     alt=""
                                 />
-                                {/* <img
-                                    className="w-fit"
-                                    src="images/sponsors/eventcenter.png"
-                                    alt=""
-                                /> */}
                                 <img
-                                    className="w-fit h-fit"
+                                    className="w-20 h-5"
                                     src="images/sponsors/Talent.png"
                                     alt=""
                                 />
                                 <img
-                                    className="w-fit md:ml-5 h-5 "
+                                    className="w-fit md:ml-2 h-5"
                                     src="images/media/FiestAR.png"
                                     alt=""
                                 />
-                                {/* <img
-                                    className="w-fit md:ml-5"
-                                    src="images/sponsors/eventnasional.png"
-                                    alt=""
-                                /> */}
-                            </div>
-                            <div className="flex mx-5 my-0 md:justify-center justify-evenly">
-                                                                <img
-                                    className="w-fit"
+                                <img
+                                    className="w-fit h-5"
                                     src="images/sponsors/eventnasional.png"
                                     alt=""
                                 />
-                                {/* <img
-                                    className="w-fit md:mr-5 h-fit"
-                                    src="images/sponsors/Talent.png"
-                                    alt=""
-                                /> */}
                                 <img
-                                    className="w-fit mx-2"
+                                    className="w-fit mx-2 h-5"
                                     src="images/sponsors/eventcenter.png"
                                     alt=""
                                 />
-                                {/* <img
-                                    className="w-fit h-fit"
-                                    src="images/sponsors/Talent.png"
-                                    alt=""
-                                /> */}
                                 <img
-                                    className="w-fit"
+                                    className="w-fit h-5"
                                     src="images/media/RadioU.png"
                                     alt=""
                                 />
-                                {/* <img
-                                    className="w-fit md:ml-5"
-                                    src="images/sponsors/eventnasional.png"
-                                    alt=""
-                                /> */}
                             </div>
-                            {/* <img
-                                className="mx-auto my-"
-                                src="images/sponsors/Talent.png"
-                                alt=""
-                            /> */}
                         </div>
                         <div className="w-full sm:ml-2 bg-[#FFF9EE] rounded-[50px] rounded-tl-[30px] rounded-tr-[30px] h-60 flex justify-center">
                             <div>
@@ -205,7 +173,7 @@ const Sponsors = () => {
                                     Case Contributor
                                 </p>
                                 <img
-                                    className="mx-auto my-3"
+                                    className="mx-auto my-3 w-2/3"
                                     src="images/sponsors/Tanio.png"
                                     alt=""
                                 />
